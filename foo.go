f0c6652d38fb468f09fe472146331410110fbcb5
Andrey di Navaronne Gaudencio Leite <57904475+andreynavaronne@users.noreply.github.com>
2025-03-18 15:30:22 -0300

    Add files via upload

---
+++ foo.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	   "fmt"
+       )
+
+func fib(n int) int {
+	if n <= 1{
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+
+}
+
+func main(){
+    go status()
+	result := fib(5)
+	fmt.Println(result)
+	
+	
+}
